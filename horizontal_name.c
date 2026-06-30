b1a12c7962c21b08f38e7215c5126b739625d528
TechiePavitra <techiepavitra@gmail.com>
2026-06-30 13:32:48 +0530

    Create horizontal_name.c to print names
    Fix syntax errors and improve formatting.

---
+++ horizontal_name.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // standard input output library
+#include<conio.h> // conio library for turbo c++
+void main()
+{
+  clrslr();
+  printf("\npatil")
+  printf(\npavitra")
+  printf(\nl.)
+getch();
+}
